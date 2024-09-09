28bcbb3369f2d14152cb7699fc67205261f7ff17
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-09 21:54:03 +0530

    refactor: Add reverse functionality to todo list

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,38 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
-const topDesserts = [
-  {
-    id: '1',
-    title: 'Chocolate Cake',
-    description: 'A piece of chocolate cake',
-    image: 'https://picsum.photos/200/300/?random',
-    price: '$5.00',
-  },
-  {
-    id: '2',
-    title: 'Gulab Jamun',
-    description: 'A piece of gulab jamun',
-    image: 'https://picsum.photos/200/300/?random',
-    price: '$3.00',
-  },
-  {
-    id: '3',
-    title: 'Protein Shake',
-    description: 'A glass of protein shake',
-    image: 'https://picsum.photos/200/300/?random',
-    price: '$10.00',
-  },
-];
+const ToDo = (props) => (
+  <tr>
+    <td>
+      <label>{props.id}</label>
+    </td>
+    <td>
+      <input type='text' />
+    </td>
+    <td>
+      <label>{props.createdAt}</label>
+    </td>
+  </tr>
+);
 
 const App = () => {
-  const listItems = topDesserts.map((dessert) => {
-    const itemText = `${dessert.title} - ${dessert.price}`;
-    return <li key={dessert.id}>{itemText}</li>;
-  });
+  const [todos, setTodos] = useState([
+    { id: 'todo1', createdAt: '18:00' },
+    { id: 'todo2', createdAt: '20:30' },
+  ]);
+
+  const reverseOrder = () => {
+    // Reverse is a mutative operation, so we need to create a new array first.
+    setTodos([...todos].reverse());
+  };
+
   return (
     <div>
-      <ul>{listItems}</ul>
+      <button onClick={reverseOrder}>Reverse</button>
+
+      <table>
+        <tbody>
+          {todos.map((todo) => (
+            <ToDo key={todo.id} id={todo.id} createdAt={todo.createdAt} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
