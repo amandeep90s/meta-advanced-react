e32ebe7f4632d24220c3b4a7c6aa837499003056
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-09 21:56:07 +0530

    refactor: Update dependencies in package.json and add ListItems component

---
+++ firstapp/src/ListItems.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './index.css';
+
+const topDesserts = [
+  {
+    id: '1',
+    title: 'Chocolate Cake',
+    description: 'A piece of chocolate cake',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$5.00',
+  },
+  {
+    id: '2',
+    title: 'Gulab Jamun',
+    description: 'A piece of gulab jamun',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$3.00',
+  },
+  {
+    id: '3',
+    title: 'Protein Shake',
+    description: 'A glass of protein shake',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$10.00',
+  },
+];
+
+const ListItems = () => {
+  const listItems = topDesserts.map((dessert) => {
+    const itemText = `${dessert.title} - ${dessert.price}`;
+    return <li key={dessert.id}>{itemText}</li>;
+  });
+  return (
+    <div>
+      <ul>{listItems}</ul>
+    </div>
+  );
+};
+
+export default ListItems;
