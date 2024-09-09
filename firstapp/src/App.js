f1447b6192259695b4122df70e987de8b792ca61
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-09 19:35:46 +0530

    feat: Render a simple list component

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
-function App() {
+const topDesserts = [
+  {
+    id: '1',
+    title: 'Chocolate Cake',
+    description: 'A piece of chocolate cake',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$5.00',
+  },
+  {
+    id: '2',
+    title: 'Gulab Jamun',
+    description: 'A piece of gulab jamun',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$3.00',
+  },
+  {
+    id: '3',
+    title: 'Protein Shake',
+    description: 'A glass of protein shake',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$10.00',
+  },
+];
+
+const App = () => {
+  const listItems = topDesserts.map((dessert) => {
+    const itemText = `${dessert.title} - ${dessert.price}`;
+    return <li key={dessert.id}>{itemText}</li>;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>{listItems}</ul>
     </div>
   );
-}
+};
 
 export default App;
