472780615dd5cd095862e91470001027899abdaa
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-10 13:30:28 +0530

    feat: Working with complex data

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [restaurantName, setRestaurantName] = useState('Lemon');
+  const [greeting, setGreeting] = useState({
+    greet: 'Hello',
+    place: 'World',
+  });
+
+  const updateGreeting = () => {
+    setGreeting((prevState) => {
+      return { ...prevState, place: 'Universe' };
+    });
+  };
 
   return (
     <div>
-      <h1>{restaurantName}</h1>
+      <h1>
+        {greeting.greet}, {greeting.place}
+      </h1>
 
-      <button
-        type='button'
-        onClick={() => setRestaurantName('Little Lemon Hotel')}
-      >
-        Update Restaurant Name
+      <button type='button' onClick={updateGreeting}>
+        Update greeting
       </button>
     </div>
   );
